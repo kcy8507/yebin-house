8b5abf9a6976f8fd447771e41b4ac7cb7f6145fc
chaeyeon kim <kcy85078507@gmail.com>
2021-11-15 16:46:06 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// function scrollDisable() {
+//   $("body")
+//     .addClass("scrollDisable")
+//     .on("scroll touchmove mousewheel", function (e) {
+//       e.preventDefault();
+//     });
+// }
+// function scrollAble() {
+//   $("body").removeClass("scrollDisable").off("scroll touchmove mousewheel");
+// }
+
+$(".nav__toggle").click(function () {
+  $("#popup__nav").toggle();
+});
+$(".cancel")
+  .unbind("click")
+  .bind("click", function () {
+    $("#popup__nav").toggle();
+  });
+//스크롤 방지 기능 아직 안했음!!
